040e01fe6a1dad9ce47de0290803cae93b3d3171
Surya Sekar <surya.sekar156@gmail.com>
2021-03-17 23:34:06 +0530

    edited text styling in second page

---
+++ lib/participants_view.dart
@@ -0,0 +1,123 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'group_model.dart';
+import 'package:grouped_list/grouped_list.dart';
+
+
+class ParticipantListView extends StatefulWidget {
+  final GroupModel groupModel;
+
+  ParticipantListView({Key key, this.groupModel}) : super(key: key);
+  @override
+  ParticipantsGroupView createState() => ParticipantsGroupView();
+}
+
+class ParticipantsGroupView extends State<ParticipantListView> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+            backgroundColor: Colors.white,
+            toolbarHeight: 80.0,
+            elevation: 0.0,
+            titleSpacing: 0.0,
+            automaticallyImplyLeading: false,
+            title: Padding(
+              padding:
+              const EdgeInsets.only(top: 0.0, bottom: 10.0),
+              child: ListTile(
+                title: Text(
+                  widget.groupModel.participants.length.toString() +
+                      ' ' +
+                      'Participants',
+                    style: TextStyle(fontStyle: FontStyle.normal, color: Colors.black.withOpacity(0.6),
+                  ),
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 10.0),
+                  child: Text(widget.groupModel.team,
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20)),
+                ),
+              ),
+              // ) ,
+            )),
+        body: GroupedListView<dynamic, String>(
+          elements: widget.groupModel.participants,
+          groupBy: (element) => element.group,
+          groupComparator: (value1, value2) => value2.compareTo(value1),
+          itemComparator: (item1, item2) =>
+              item1.first_name.compareTo(item2.first_name),
+          order: GroupedListOrder.DESC,
+          groupSeparatorBuilder: (String value) => Padding(
+            padding: const EdgeInsets.only(left: 20.0),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+              child: Text(
+                value,
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                  background: Paint()
+                    ..color = Colors.yellow
+                    ..strokeWidth = 10
+                    ..style = PaintingStyle.stroke
+                    ..strokeJoin = StrokeJoin.round,
+                ),
+              ),
+            ),
+          ),
+          itemBuilder: (c, element) {
+            return Dismissible(
+                key: Key(element.id),
+                onDismissed: (direction) {
+                  setState(() {
+                    widget.groupModel.participants
+                        .removeWhere((item) => item.id == element.id);
+                  });
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text(element.first_name + " deleted")));
+                },
+                child: Card(
+                  elevation: 8.0,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0),
+                  ),
+                  margin:
+                  new EdgeInsets.symmetric(horizontal: 10.0, vertical: 6.0),
+                  child: Container(
+                      child: Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15.0), //or 15.0
+                              child: Container(
+                                height: 45.0,
+                                width: 40.0,
+                                child: Image(
+                                  image: NetworkImage(element.avatar),
+                                  fit: BoxFit.fill,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 20.0),
+                            child: Text(
+                                element.first_name + ' ' + element.last_name,
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 15)),
+                          )
+                        ],
+                      )),
+                ));
+          },
+        ));
+  }
+}
